a09a8491095ad0d4348d8e5c5f84aba0f83638eb
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-18 01:45:50 +0700

    18/12/2024

--- Main.fs
+++ Main.fs
@@ -1,29 +1,19 @@
 ﻿open System
 open System.Net
-open System.Text.Json.Serialization
 open rest_api_fs.database.Sqlite
 open rest_api_fs.handlers.User
-
-type ApiResponse = {
-    [<JsonPropertyName("status")>] Status: string
-    [<JsonPropertyName("code")>] Code: int
-}
 
 [<EntryPoint>]
 let main _ =
     initializeDatabase ()
-
     let listener = new HttpListener()
     listener.Prefixes.Add("http://localhost:6012/")
     listener.Start()
-
     let rec listenForRequests () =
         async {
             do! userHandler listener
             return! listenForRequests ()
         }
-
     listenForRequests () |> Async.Start
-
     Console.ReadLine() |> ignore
     0
